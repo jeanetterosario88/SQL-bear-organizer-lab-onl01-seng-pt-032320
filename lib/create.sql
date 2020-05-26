dc5411f2354a8e22e0082f794957763780f6ba7a
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-05-26 00:37:13 +0000

    Automatically backed up by Learn

---
+++ lib/create.sql
@@ -0,0 +1,11 @@
+CREATE TABLE bears (
+        id INTEGER PRIMARY KEY,
+                id INTEGER,
+                name TEXT,
+                age INTEGER,
+                gender TEXT,
+                color TEXT,
+                temperament TEXT,
+                alive INTEGER
+            );
+);
